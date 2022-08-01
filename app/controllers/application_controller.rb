c15e924fe3fb909f8bc6df7267c1726791205604
JaiVon <frederickjaivon@yahoo.com>
2022-08-01 13:28:14 -0400

    first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,9 @@ class ApplicationController < Sinatra::Base
   
   # Add your routes here
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+    { message: "We started the server!" }.to_json
   end
+
+  ## http://localhost:9292/
 
 end
